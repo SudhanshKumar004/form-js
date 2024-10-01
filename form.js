cfafac4a2eff7454c0e70a9adab0e84ddef0b988
Sudhansh Kumar <sudhanshkb04@gmail.com>
2024-10-01 19:58:06 +0530

    third commit

--- form.js
+++ form.js
@@ -48,6 +48,7 @@ function data(){
         return false;
     } 
 
+
     
     // if(name === ""){
     //     window.alert("Please Enter Your Name");
@@ -72,16 +73,16 @@ function s(){
     let selectedinput = document.querySelector('#name');
     let selecterrorname = document.querySelector('#errorname');
     selecterrorname.innerHTML = "";
-    selectedinput.style.borderColor = "black";
-    selecterrorname.style.outlineColor = "black";
+    selectedinput.style.borderColor = "blueviolet";
+    selecterrorname.style.outlineColor = "blueviolet";
 }
 
 function s2(){
     let selectedinput2 = document.querySelector('#email');
     let selecterrorname2 = document.querySelector('#errorname2');
     selecterrorname2.innerHTML = "";
-    selectedinput2.style.borderColor = "black";
-    selecterrorname2.style.outlineColor = "black";    
+    selectedinput2.style.borderColor = "blueviolet";
+    selecterrorname2.style.outlineColor = "blueviolet";    
 
 }
 
@@ -89,8 +90,8 @@ function s3(){
     let selectedinput3 = document.querySelector('#number');
     let selecterrorname3 = document.querySelector('#errorname3');
     selecterrorname3.innerHTML = "";
-    selectedinput3.style.borderColor = "black";
-    selecterrorname3.style.outlineColor = "black";    
+    selectedinput3.style.borderColor = "blueviolet";
+    selecterrorname3.style.outlineColor = "blueviolet";    
 
 }
 
@@ -98,8 +99,8 @@ function s4(){
     let selectedinput4 = document.querySelector('#psw');
     let selecterrorname4 = document.querySelector('#errorname4');
     selecterrorname4.innerHTML = "";
-    selectedinput4.style.borderColor = "black";
-    selecterrorname4.style.outlineColor = "black";    
+    selectedinput4.style.borderColor = "blueviolet";
+    selecterrorname4.style.outlineColor = "blueviolet";    
 
 }
 
@@ -107,7 +108,8 @@ function s5(){
     let selectedinput5 = document.querySelector('#psw2');
     let selecterrorname5 = document.querySelector('#errorname5');
     selecterrorname5.innerHTML = "";
-    selectedinput5.style.borderColor = "black";
-    selecterrorname5.style.outlineColor = "black";    
+    selectedinput5.style.borderColor = "blueviolet";
+    selecterrorname5.style.outlineColor = "blueviolet";    
 
 }
+
